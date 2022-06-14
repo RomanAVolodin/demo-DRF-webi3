9445a18bb1c8b28e8b8b5eec7619c36d9d26daf7
Roman Volodin <rozarioagro@gmail.com>
2022-06-14 17:18:08 +0300

    added post, put, patch and delete methods

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,3 +2,4 @@ from rest_framework import serializers
 
 
 class HelloSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=10)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         an_apiview = [
             'Uses HTTP methods',
@@ -12,3 +17,21 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
